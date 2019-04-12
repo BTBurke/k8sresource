8f8061d3e94665528938521fe48fc7adda128ffa
Bryan Burke <bryan.t.burke@gmail.com>
2019-04-12 08:50:11 -0400

    first commit

---
+++ memcalc.go
@@ -0,0 +1,117 @@
+// Package memcalc provides utility functions for working with Kubernetes-style memory resources
+// which are expressed as strings such as 256Mi or 2Gi.  Methods are provided for converting
+// between string representations and floats, and for math operations.
+package memcalc
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+const (
+	_ = iota
+	_
+	// Mi = Megabytes
+	Mi float64 = 1 << (10 * iota)
+	// Gi = Gigabytes
+	Gi
+)
+
+// Memory allows converstion between string and float representations and basic math operations on memory types
+type Memory interface {
+	Add(m string) (Memory, error)
+	AddF(m float64) Memory
+	Sub(m string) (Memory, error)
+	SubF(m float64) Memory
+	ToString() string
+	ToFloat64() float64
+}
+
+type memory struct {
+	m float64
+}
+
+// New returns a new memory instance initialized at 0
+func New() Memory {
+	return memory{0}
+}
+
+// NewFromString parses a Kubernetes-style memory string (e.g., 256Mi, 1Gi)
+func NewFromString(m string) (Memory, error) {
+	f, err := memToFloat64(m)
+	if err != nil {
+		return nil, err
+	}
+	return memory{f}, nil
+}
+
+// NewFromFloat creates a new memory instance initialized to m
+func NewFromFloat(m float64) Memory {
+	return memory{m}
+}
+
+// Add will parse the memory expressed as a string and return a new memory instance
+// equal to the sum of the current instance plus m
+func (s memory) Add(m string) (Memory, error) {
+	f, err := memToFloat64(m)
+	if err != nil {
+		return nil, err
+	}
+	return memory{s.m + f}, nil
+}
+
+// Sub will parse the memory expressed as a string and return a new memory instance
+// equal to the current instance minus m
+func (s memory) Sub(m string) (Memory, error) {
+	f, err := memToFloat64(m)
+	if err != nil {
+		return nil, err
+	}
+	return memory{s.m - f}, nil
+}
+
+// AddF will return a new memory instance equal to the sum of the current instance plus m
+func (s memory) AddF(m float64) Memory {
+	return memory{s.m + m}
+}
+
+// Add will return a new memory instance equal to the current instance minus m
+func (s memory) SubF(m float64) Memory {
+	return memory{s.m - m}
+}
+
+// ToString returns the Kubernetes-style memory value as a string rounded up to the nearest
+// megabyte.  Values over 1Gi will still be returned as an equivalent Mi value.
+func (s memory) ToString() string {
+	return float64ToMi(s.m)
+}
+
+// ToFloat returns the memory value as a float
+func (s memory) ToFloat64() float64 {
+	return s.m
+}
+
+func memToFloat64(s string) (float64, error) {
+	switch {
+	case strings.HasSuffix(s, "Mi"):
+		mem, err := strconv.ParseFloat(strings.TrimSuffix(s, "Mi"), 64)
+		if err != nil {
+			return 0, fmt.Errorf("failed to convert memory string %s to float", s)
+		}
+		return mem * Mi, nil
+	case strings.HasSuffix(s, "Gi"):
+		mem, err := strconv.ParseFloat(strings.TrimSuffix(s, "Gi"), 64)
+		if err != nil {
+			return 0, fmt.Errorf("failed to convert memory string %s to float", s)
+		}
+		return mem * Gi, nil
+	default:
+		return 0, fmt.Errorf("failed to convert memory string %s to float, unknown units", s)
+	}
+}
+
+func float64ToMi(m float64) string {
+	return fmt.Sprintf("%dMi", int(math.Ceil(m/Mi)))
+}
---
+++ memcalc_test.go
@@ -0,0 +1,100 @@
+package memcalc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertStringToFloat(t *testing.T) {
+	tcs := []struct {
+		Name      string
+		Value     string
+		Expected  float64
+		ShouldErr bool
+	}{
+		{Name: "megabytes < 1Gi", Value: "256Mi", Expected: 256 * Mi, ShouldErr: false},
+		{Name: "1Gi", Value: "1Gi", Expected: 1 * Gi, ShouldErr: false},
+		{Name: "10Gi", Value: "10Gi", Expected: 10 * Gi, ShouldErr: false},
+		{Name: "1024Mi", Value: "1024Mi", Expected: 1 * Gi, ShouldErr: false},
+		{Name: "1500Mi", Value: "1500Mi", Expected: 1500 * Mi, ShouldErr: false},
+		{Name: "Invalid suffix", Value: "1500Xi", Expected: 0, ShouldErr: true},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			f, err := memToFloat64(tc.Value)
+			switch tc.ShouldErr {
+			case true:
+				assert.Error(t, err)
+			default:
+				assert.NoError(t, err)
+				assert.Equal(t, tc.Expected, f)
+			}
+		})
+	}
+}
+
+func TestConvertFloatToString(t *testing.T) {
+	tcs := []struct {
+		Name     string
+		Value    float64
+		Expected string
+	}{
+		{Name: "megabytes < 1Gi", Expected: "256Mi", Value: 256 * Mi},
+		{Name: "1Gi", Expected: "1024Mi", Value: 1 * Gi},
+		{Name: "10Gi", Expected: "10240Mi", Value: 10 * Gi},
+		{Name: "1024Mi", Expected: "1024Mi", Value: 1024 * Mi},
+		{Name: "1500Mi", Expected: "1500Mi", Value: 1500 * Mi},
+		{Name: "-256Mi", Expected: "-256Mi", Value: -256 * Mi},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			f := float64ToMi(tc.Value)
+			assert.Equal(t, tc.Expected, f)
+		})
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tcs := []struct {
+		Name     string
+		Value1   string
+		Value2   string
+		Expected float64
+	}{
+		{Name: "less than 1Gi", Value1: "256Mi", Value2: "256Mi", Expected: 512 * Mi},
+		{Name: "greater than 1Gi", Value1: "256Mi", Value2: "1Gi", Expected: 256*Mi + 1*Gi},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			m, err := NewFromString(tc.Value1)
+			assert.NoError(t, err)
+			mAdded, err := m.Add(tc.Value2)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.Expected, mAdded.ToFloat64())
+		})
+	}
+}
+
+func TestSub(t *testing.T) {
+	tcs := []struct {
+		Name     string
+		Value1   string
+		Value2   string
+		Expected float64
+	}{
+		{Name: "zero", Value1: "256Mi", Value2: "256Mi", Expected: 0},
+		{Name: "less than 1Gi", Value1: "256Mi", Value2: "128Mi", Expected: 128 * Mi},
+		{Name: "greater than 1Gi", Value1: "1Gi", Value2: "512Mi", Expected: 512 * Mi},
+		{Name: "negative", Value1: "512Mi", Value2: "1024Mi", Expected: -512 * Mi},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			m, err := NewFromString(tc.Value1)
+			assert.NoError(t, err)
+			mAdded, err := m.Sub(tc.Value2)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.Expected, mAdded.ToFloat64())
+		})
+	}
+}
